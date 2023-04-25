0078af788292660d01a219b006aa08b736f4a603
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-04-25 12:00:48 +0200

    add click cell

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,17 @@ const eleGrid = document.querySelector('.grid');
 
 createGrid(100, eleGrid);
 
+const listCells = document.querySelectorAll('.cell');
+for (let i = 0; i < listCells.length; i++) {
+	const cell = listCells[i];
+	cell.addEventListener('click',
+		function colorCell() {
+			console.log('cliccata cella n°', i + 1);
+			this.classList.toggle('clicked');
+		}
+	);
+}
+
 // FUNCTION
 
 function createGrid(numCells, eleContainer) {
